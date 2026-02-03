90eed1828df353fabe826e24e0eb1723ae4dd497
keshav <keshav.acewebx@gmail.com>
2026-02-03 17:36:06 +0530

    init commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User::all();
+        return response()->json($users); 
+    }
+
+    public function special_user($id){
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        return $user;
+    }
+}
